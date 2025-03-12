a08b323c4da2e4fa0d304dcb5da7d122875786dc
Dmitry tswr Kornev <d.v.kornev@gmail.com>
2025-03-12 01:36:35 +0100

    Day 12: first working poi example

--- day12/blink.cpp
+++ day12/blink.cpp
@@ -24,6 +24,8 @@
 #include "hardware/pwm.h"
 #include "pico/stdlib.h"
 
+#include "ws2812.pio.h"
+
 class Led {
 public:
   explicit Led(int pin) : pin_(pin) {
@@ -548,7 +550,7 @@ private:
   int pin_;
 };
 
-int main() {
+int mainNN() {
   stdio_init_all();
 
   // DS18B20 sensor(26);
@@ -589,4 +591,22 @@ int main() {
   }
 
   return 0;
+}
+
+int main() {
+  stdio_init_all();
+  PIO pio;
+  uint sm = 0;
+  uint offset;
+  bool success = pio_claim_free_sm_and_add_program_for_gpio_range(
+      &ws2812_program, &pio, &sm, &offset, 25, 1, true);
+  hard_assert(success);
+  ws2812_program_init(pio, sm, offset, 25);
+
+  while (true) { /* Loop forever */
+    pio_sm_put_blocking(pio, sm, 1);
+    sleep_ms(500);
+    pio_sm_put_blocking(pio, sm, 0);
+    sleep_ms(500);
+  }
 }
